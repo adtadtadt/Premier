334a2fcbbaa85e42aa15e0516ef07b729b031687
adt <adt@L302P06.pdg.esi.heb.site>
2013-09-10 10:13:08 +0200

    Modif 2.2

--- src/test/Test.java
+++ src/test/Test.java
@@ -15,6 +15,6 @@ public class Test {
      */
     public static void main(String[] args) {
         // TODO code application logic here  Modif1
-        //Modif2
+        //Modif2     dsfgdqsrgsdre
     }
 }
